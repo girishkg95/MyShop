3a4719a4fc3620e4a3e251db9cc5d94a6bbb5b4c
girishkg95 <34678175+girishkg95@users.noreply.github.com>
2020-03-25 13:14:27 +0530

    Homecontroller file is modified

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace MyShop.WebUI.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page!";
 
             return View();
         }
